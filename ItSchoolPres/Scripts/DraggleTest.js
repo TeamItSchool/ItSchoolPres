a871a3ca4f7fdc7312f129b45c011186413f1eb6
Kikabou Guenole <kikabouguenole@gmail.com>
2015-11-02 14:57:26 +0100

    Initialization + starting the application DragAndDrop

---
+++ ItSchoolPres/Scripts/DraggleTest.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    var $draggable = $('.draggable').draggabilly();
+    var p = $("p:first");
+    var position = p.position();
+    $("p:last").text("left: " + position.left + ", top: " + position.top);
+});
